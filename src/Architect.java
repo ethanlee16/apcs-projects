5c871d7a0cee669dc7bb23f0d222b1252c6e8f7b
Timothy Gu <timothygu99@gmail.com>
2014-10-16 16:30:11 -0700

    Architect: some useless microoptimizations
    This commit is just to test ISC license is OK, and that GitHub works.

--- src/Architect.java
+++ src/Architect.java
@@ -6,7 +6,23 @@
  * 0.2 puts the whole roof on
  * 1.0 actually builds the whole house!
  * 
- * @author ethan.lee
+ * @author ethan.lee and Tiancheng "Timothy" Gu
+ *
+ * Modifications by Timothy Gu are mad available under the ISC license:
+ *
+ * Permission to use, copy, modify, and/or distribute this software for
+ * any purpose with or without fee is hereby granted, provided that the
+ * above copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ * WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE
+ * FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY
+ * DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
+ * IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * 
+ * END ISC LICENSE
  *
  */
 import java.util.*;
@@ -22,39 +38,42 @@ public class Architect {
 				+ "The minimum and standard is seven.");
 		
 		//Loops until width is a valid response, Maths all the things after width is actually determined
-		double width = prompt.nextDouble();	
-		while (width % 2 == 0 || width < 7){System.out.println("Error: number must be at "
-				+ "least 7 and odd. Try again:  ");width = prompt.nextDouble();}
+		int width = prompt.nextInt();	
+		while ((width & 1) == 0 || width < 7){
+			System.out.println("Error: number must be at "
+				+ "least 7 and odd. Try again:  ");
+			width = prompt.nextDouble();
+		}
 		
-		double half = Math.floor(width/2);
-		double fourth = Math.ceil(width/4);
-		double eighth = Math.floor(fourth/2);
+		int half   = (width  >> 1);
+		int fourth = (half   >> 1);
+		int eighth = (fourth >> 1);
 		String equals = "";
 		
 		//First star for top of roof is about halfway
-		for(int count = (int) half; count > 0; count--){
+		for(int count = half; count > 0; count--){
 			houseStr.append(" ");
 		}
 		houseStr.append("*\n");
 		
 		//Seconds stars are about 1/4 of the way
-		for (int count = (int) fourth; count > 0; count--){
+		for (int count = fourth; count > 0; count--){
 			houseStr.append(" ");
 		}
 		houseStr.append("*");
 		
 		//Accounts for blanks in between two mid-stars
-		for (int count = (int) (width - (fourth*2) - 2); count > 0; count--) {
+		for (int count = (width - half - 2); count > 0; count--) {
 			houseStr.append(" ");
 		}
 		houseStr.append("*\n");
 		
-		for (int count = (int) eighth; count > 0; count--){
+		for (int count = eighth; count > 0; count--){
 			houseStr.append(" ");
 		}
 		houseStr.append("*");
 		
-		for (int count = (int) (width - (eighth*2) - 2); count > 0; count--) {
+		for (int count = (int) (width - half - 2); count > 0; count--) {
 			houseStr.append(" ");
 		}
 		houseStr.append("*\n");
@@ -62,7 +81,7 @@ public class Architect {
 		// Reusable code: the top two rows for the house are the same idea:
 		// beginning symbol, character all the way across, and end symbol.
 
-		for (int count = (int) width - 2; count > 0; count--){
+		for (int count = width - 2; count > 0; count--){
 			equals += "-";
 		}
 		houseStr.append("+" + equals + "+\n");
@@ -78,7 +97,7 @@ public class Architect {
 		// For example: +---+-+---+ makes up the bottom of the house, and |   .-.   | makes
 		// the top of the door frame.
 		
-		for (int count = (int) half - 2; count > 0; count--){
+		for (int count = half - 2; count > 0; count--){
 			equals += " ";
 		}
 		houseStr.append("|" + equals + ".-." + equals + "|\n");
