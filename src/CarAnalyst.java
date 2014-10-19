1c0c48ddefd0b13e11b657494555996bf1c7d906
Ethan Lee <ethanlee@ethansites.com>
2014-10-18 23:41:37 -0700

    Beginning CarAnalyst + testing Eclipse Git implementation

---
+++ src/CarAnalyst.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * CarAnalyst.java - helps you decide whether to get a hybrid or a gas-guzzler
+ * @author ethan.lee
+ *
+ */
+import java.util.*;
+public class CarAnalyst {
+	private static Scanner prompt = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+	}
+}
