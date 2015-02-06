2e4c4594f44ba4048aa5408a9934d8bd2a8bbdef
Ethan Lee <ethanlee@ethansites.com>
2015-02-06 06:27:16 -0800

    Finishes ZIPCodeEncoder

---
+++ src/Chapter 8 - Designing Classes/Cannonball.java
@@ -0,0 +1,11 @@
+
+public class Cannonball {
+	
+	private double x, y = 0;
+	private double xVelocity = 0;
+	private double yVelocity = 0;
+	
+	public Cannonball(double xPos) {
+		
+	}
+}
--- src/Chapter 8 - Designing Classes/ZIPCodeEncoder.java
+++ src/Chapter 8 - Designing Classes/ZIPCodeEncoder.java
@@ -2,38 +2,97 @@
  * ZIPCodeEncoder.java
  * @author ethan
  *
+ * TODO: 
+ * - separate ZIPCodeEncoder behavior from
+ *   actual JPanel to reduce redundancy.
+ * x needs to exit after last dialog 
+ *   after providing bar code [solved]
+ * x the logic involving encodedInput is
+ *   literally a hack. efficiency could
+ *   be greatly improved. [solved]
  */
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import java.awt.datatransfer.*;
 
 @SuppressWarnings("serial")
-public class ZIPCodeEncoder extends JPanel {
+public class ZIPCodeEncoder extends JPanel implements ActionListener {
 	
 	private static String input;
+	private String encodedInput = "";
 	private int place = 0;
 	
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
 		//frame.setLayout(new FlowLayout());
 		input = JOptionPane.showInputDialog(frame, "Enter a ZIP Code or a bar code "
-				+ "(full bars as '|' and half bars as ':')");
+				+ "(full bars as '|' and half bars as ':'. Be sure to put full bars "
+				+ "between the entire code.)");
+		
+		// A SAMPLE THAT CAN BE USED FOR TESTING IS: ||:|:::|:|:||::::::||:|::|:::||| //
+		// SHOULD VALIDATE TO 95014 WITH CHECK DIGIT VALIDATION //
+		
 		if(input.matches("\\d\\d\\d\\d\\d")) {
-			frame.getContentPane().add(new ZIPCodeEncoder());
+			ZIPCodeEncoder encoder = new ZIPCodeEncoder();
+			encoder.setBounds(0, 0, 220, 105);
+			frame.getContentPane().add(encoder);
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.setSize(220, 105);
+			frame.setLayout(null);
+			
+			JButton copy = new JButton("Copy bar code to clipboard");
+			copy.addActionListener(encoder);
+			copy.setBounds(15, 70, 200, 25);
+			frame.getContentPane().add(copy);
+			
+			frame.setSize(220, 125);
 			frame.setVisible(true);
 		}
 		else {
-			
+			ZIPCodeEncoder encoder = new ZIPCodeEncoder();
+			// To get rid of opening and closing bars
+			input = input.substring(1, input.length() - 1);
+			input = input.replace('|', '1');
+			input = input.replace(':', '0');
+	
+			// Regex from http://stackoverflow.com/questions/
+			// 22283856/splitting-string-into-n-length-elements-for-an-array
+			String[] separatedNums = input.split("(?<=\\G\\d{5})");
+			String zipNums = "";
+			for(int i = 0; i < 6; i++) {
+				zipNums += encoder.decodeNumber(separatedNums[i]);
+			}
+			int checkDigit = Integer.valueOf(zipNums.substring(zipNums.length() - 1, zipNums.length()));
+			int sum = 0;
+			int zip = Integer.valueOf(zipNums.substring(0, zipNums.length() - 1));
+			while(zip != 0) {
+				sum += zip % 10;
+				zip /= 10;
+			}
+			if((sum + checkDigit) % 10 != 0) {
+				JOptionPane.showMessageDialog(null, "The check digit was incorrect. Extracted ZIP was " 
+						+ zipNums.substring(0, zipNums.length() - 1));
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "The check digit verified, and the ZIP was "
+						+ zipNums.substring(0, zipNums.length() - 1));
+			}
+			System.exit(0); // solves close dialog, could also be break; return;
 		}
 		
 	}
 	
 	public void paintComponent(Graphics g) {
+		// paintComponent is constantly called
+		// setting encodedInput back to empty
+		// corrects redundancy issue
+		
+		encodedInput = "";
 		char[] zip = input.toCharArray();
 		for(char c : zip) {
 			char[] digit = encodeNumber(Character.getNumericValue(c)).toCharArray();
 			for (char d : digit) {
+				encodedInput += d;
 				makeBar(g, d);
 				place++;
 			}
@@ -47,10 +106,15 @@ public class ZIPCodeEncoder extends JPanel {
 		
 		char[] checkDigit = encodeNumber(max - sum).toCharArray();
 		for(char c : checkDigit) {
+			encodedInput += c;
 			makeBar(g, c);
 			place++;
 		}
-		//JLabel label = new JLabel(input);
+		// These are the guiding bars for all codes (beginning and end)
+		g.fillRect(46, 47, 2, 6);
+		g.fillRect(170, 47, 2, 6);
+		// This is the ZIP code as text
+		g.drawString(input, 46, 40);
 	}
 	
 	public void makeBar(Graphics g, char d) {
@@ -118,5 +182,10 @@ public class ZIPCodeEncoder extends JPanel {
 		}
 	}
 	
-	
+	public void actionPerformed(ActionEvent event) {
+		Clipboard clip = Toolkit.getDefaultToolkit().getSystemClipboard();
+		clip.setContents(new StringSelection("|" + encodedInput
+				.replace('1', '|').replace('0', ':') + "|"), null);
+		JOptionPane.showMessageDialog(null, "Got it! The code was copied.");
+	}
 }
